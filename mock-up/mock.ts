52a95fdc57043ff6d345e48cba7dab667e0e1249
Ehis Akhile <akhileehis@gmail.com>
2023-03-22 17:03:50 +0100

    Vanus Templates
    Signed-off-by: Ehis Akhile <akhileehis@gmail.com>

---
+++ mock-up/mock.ts
@@ -0,0 +1,40 @@
+[
+
+{
+        sinkIcon: "discord", 
+        source: "github", 
+        sink:"http", 
+        "description": "The maintenance team of the GitHub repo can receive real-time notifications on Discord when someone gives a star to their repo.", 
+        "subTitle": "GitHub star event triggers a Discord message", 
+        "id": "20230320_1"
+},
+
+{
+        sinkIcon: "discord", 
+        source: "github", 
+        sink:"http", 
+        "description": "The maintenance team of the GitHub repo can receive real-time notifications on Discord when someone creates an issue on their repo.", 
+        "subTitle": "GitHub issue event triggers a Discord message", 
+        "id": "20230320_2"
+},
+
+{
+        sinkIcon: "discord", 
+        source: "github", 
+        sink:"http", 
+        "description": "The maintenance team of the GitHub repo can receive real-time notifications on Discord when someone creates an issue comment on their repo.", 
+        "subTitle": "GitHub issue comment event triggers a Discord message", 
+        "id": "20230320_3"
+},
+
+{
+        sinkIcon: "discord",
+        source: 'github', 
+        sink:'http', 
+        "description": "The maintenance team of the GitHub repo can receive real-time notifications on Discord when anyone opens a pull-request.", 
+        "subTitle": "GitHub opened pull-request event triggers a Discord message", 
+        "id": '20230321_1'
+}
+
+    
+]
