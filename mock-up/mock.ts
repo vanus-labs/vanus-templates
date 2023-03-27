487a30be2b20948db1b06b6fe810b1c4183068d4
Jeffrey Whewhetu <jeffwhewhetu@gmail.com>
2023-03-27 15:50:17 +0100

    Add template of loginCount for Feishu

--- mock-up/mock.ts
+++ mock-up/mock.ts
@@ -63,4 +63,13 @@
         "id": '20230326_1'
 }
 
+{
+        sinkIcon: "auth0",
+        source: 'webhook',
+        sink: 'http',
+        "description": "The Auth0 Client can recieve real-time notifications of how many times a user is logged in to their account with the associated IP address",
+        "subTitle": "Auth0 loginCount event triggers a Feishu message with login counts and associated IP address.",
+        "id": '20230327_1'
+}
+
 ]
